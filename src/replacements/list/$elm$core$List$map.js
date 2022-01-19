efe7efcef779fb2f511b84666f7d1b11044d9b2c
Wheatwizard <ejolive97@gmail.com>
2022-01-19 22:33:29 +0100

    Optimized map further with loop unrolling.

--- src/replacements/list/$elm$core$List$map.js
+++ src/replacements/list/$elm$core$List$map.js
@@ -1,10 +1,15 @@
-var $elm$core$List$map = F2(function (f, xs) {
-  var tmp = _List_Cons(undefined, _List_Nil);
-  var end = tmp;
-  for (; xs.b; xs = xs.b) {
-    var next = _List_Cons(f(xs.a), _List_Nil);
-    end.b = next;
-    end = next;
-  }
-  return tmp.b;
-});
+var $elm$core$List$map = F2(
+  function (f, xs) {
+	  if (xs.b) {
+		  var result = _List_Cons(f(xs.a), _List_Nil);
+		  var final = result;
+		  xs = xs.b;
+		  for (;xs.b; xs = xs.b) {
+		    final.b = _List_Cons(f(xs.a), _List_Nil);
+		    final = final.b;
+		  }
+		  return result;
+	  } else {
+		  return _List_Nil;
+	  }
+  });
